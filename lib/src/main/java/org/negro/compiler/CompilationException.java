8bb6cb6e84971383013f3fe44226091b0cc37b68
roJluU-TTporpAMMuCT <102484646+roJluU-TTporpAMMuCT@users.noreply.github.com>
2022-07-05 22:41:06 +0300

    Delete lib directory

--- lib/src/main/java/org/negro/compiler/CompilationException.java
+++
@@ -1,8 +0,0 @@
-package org.negro.compiler;
-
-public class CompilationException extends RuntimeException {
-	private static final long serialVersionUID = 5272588827551900536L;
-
-	public CompilationException(String msg) { super(msg); }
-
-}
